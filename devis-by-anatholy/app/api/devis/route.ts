b6f8351718805a17c7e191da33e701a6b99785d6
Anatholy Bricon <anatholyb@gmail.com>
2024-11-02 16:02:18 +0100

    delete handler on api/devis

--- devis-by-anatholy/app/api/devis/route.ts
+++ devis-by-anatholy/app/api/devis/route.ts
@@ -18,13 +18,3 @@ export async function GET() {
   const devis = await prisma.devis.findMany();
   return NextResponse.json(devis, { status: 200 });
 }
-
-export async function handler(req: NextRequest) {
-  if (req.method === 'POST') {
-    return POST(req);
-  } else if (req.method === 'GET') {
-    return GET();
-  } else {
-    return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
-  }
-}
